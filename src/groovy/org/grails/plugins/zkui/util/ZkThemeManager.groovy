27f75a2e1c93e4bbdefb5cc7354ed1b0d3a5ad9f
jlsgomes1 <47191538+jlsgomes1@users.noreply.github.com>
2019-01-31 12:21:53 -0200

    Features ZkThemeManager
    used for dynamic switch the current theme. Each jar file theme should be on lib folder.

---
+++ src/groovy/org/grails/plugins/zkui/util/ZkThemeManager.groovy
@@ -0,0 +1,36 @@
+package org.grails.plugins.zkui.util
+
+import javax.servlet.http.HttpServletRequest
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import org.zkoss.zk.ui.*
+import org.zkoss.zul.*
+import org.zkoss.zk.ui.util.*
+import org.zkoss.zk.ui.event.*
+import org.zkoss.zk.ui.ext.*
+import org.zkoss.zk.au.*
+import org.zkoss.zk.au.out.*
+import org.zkoss.zul.theme.*
+
+/**
+ *
+ * @author jlsgomes1@gmail.com
+ */
+class ZkThemeManager {
+    private switchTheme(String themeName){
+        if (themeName.equals("") || themeName == null){
+           themeName = "iceblue"  // default ZK Theme
+        }
+        Themes.setTheme(Executions.getCurrent(), themeName)
+        Executions.sendRedirect("");
+    }
+    
+    String getCurrentThemeName(){
+        def cookieThemeResolver = new CookieThemeResolver()
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = attr.getRequest();
+        return cookieThemeResolver.getTheme(request)
+    }
+}
+
+
